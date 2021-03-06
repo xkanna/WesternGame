ecd568219489cb611ac275f2c35a569db4b8f955
Katarina Veljkovic <40143205+xkanna@users.noreply.github.com>
2021-03-06 19:56:48 +0100

    Character and basic movement added
    Cowboy character added with up, down, left and right movement and animations. Few cactus images and snake added.

---
+++ WesternGame/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public Animator animator;
+    public float speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+
+
+        transform.position = transform.position + movement * Time.deltaTime * speed;
+
+    }
+}
