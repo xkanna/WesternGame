cde8221f11ca9c5442149f12a38f45972ac66917
Katarina Veljkovic <40143205+xkanna@users.noreply.github.com>
2021-03-07 17:07:15 +0100

    Set camera movement and background
    Camera follows the player, and background set with tiles

---
+++ WesternGame/Assets/Scripts/Camera Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private Transform cowboyTransform;
+
+    void Start()
+    {
+        cowboyTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    
+
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 currentposition = transform.position;
+
+        currentposition.x = cowboyTransform.position.x;
+        currentposition.y = cowboyTransform.position.y;
+
+        transform.position = currentposition;
+    }
+}
