f6e18db9a62f3fb24bdbfb71438a302e93f7e834
Katarina Veljkovic <40143205+xkanna@users.noreply.github.com>
2021-03-07 23:23:26 +0100

    Shooting implemented
    Added gun and implemented shooting bullets

---
+++ WesternGame/Assets/Scripts/Player Scripts/AimWeapon.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AimWeapon : MonoBehaviour
+{
+    private Transform aimTransform;
+    private Transform aimGunEndPointTransform;
+    private Animator aimAnimator;
+
+    public event EventHandler<OnShootEventArgs> onShoot;
+
+    public class OnShootEventArgs : EventArgs
+    {
+        public Vector3 gunEndPointPosition;
+        public Vector3 shootPosition;
+    }
+
+    private void Awake()
+    {
+        aimTransform = transform.Find("Aim");
+        aimGunEndPointTransform = aimTransform.Find("GunEndPointPosition");
+    }
+
+    private void Update()
+    {
+        HandleAiming();
+        HandleShooting();
+    }
+
+    private void HandleAiming()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f;
+
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    private void HandleShooting()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //aimAnimator.SetTrigger("Shoot");
+
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0f;
+
+            onShoot?.Invoke(this, new OnShootEventArgs
+            {
+                gunEndPointPosition = aimGunEndPointTransform.position,
+                shootPosition = mousePosition,
+            });
+        }
+
+    }
+}
---
+++ WesternGame/Assets/Scripts/Player Scripts/Shooting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firepoint;
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bullet, firepoint.position, firepoint.rotation);
+
+    }
+}
