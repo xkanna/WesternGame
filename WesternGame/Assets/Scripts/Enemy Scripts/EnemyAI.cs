7bb55e02f3243634577968bf956a5df6e4670c38
Katarina Veljkovic <40143205+xkanna@users.noreply.github.com>
2021-03-08 16:17:47 +0100

    Enemy follow implemented
    Enemy now chases the player if in range.

---
+++ WesternGame/Assets/Scripts/Enemy Scripts/EnemyAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    
+    private float speed = 1f;
+    private float maximumDistance = 0.3f;
+    private float minimumDistance = 5f;
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+    
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) > maximumDistance && Vector2.Distance(transform.position, target.position) < minimumDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        
+    }
+    
+}
